ea6af8050730f30376ea1891993c54d451c6bc70
ChristinaAshCruz <christina.ash.cruz@gmail.com>
2023-06-10 23:46:06 -0700

    navbar scss updates

--- responsive_web_design/portfolio_website/frontend_react/src/components/Navbar/Navbar.jsx
+++ responsive_web_design/portfolio_website/frontend_react/src/components/Navbar/Navbar.jsx
@@ -10,9 +10,12 @@ const Navbar = () => {
         <img src={images.logo} alt="logo" />
       </div>
       <ul className="app__navbar-links">
+        {/* will be loopin through our links in our nav bar */}
+        {/* organizing those links into an ARRAY */}
+        {/* for each one of those items, we are going to return a list item */}
         {["home", "about", "work", "skills", "contact"].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
-            <div />
+            {/* the link will redirect to that SECTION of the page */}
             <a href={`#${item}`}>{item}</a>
           </li>
         ))}
