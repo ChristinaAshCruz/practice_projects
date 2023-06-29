c5379ea3021f8b029d874349ecef9af663bb2f15
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-06-29 13:53:24 -0700

    added assets + navbar progress + fixed tailwind

--- react/react_admin_dashboard/src/App.js
+++ react/react_admin_dashboard/src/App.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  // here, we are setting the activeMenu to True
+  const activeMenu = true;
+
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ background: "blue", borderRadius: "50%" }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+          {/* this checks whether the menu is active or not */}
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+              Sidebar
+            </div>
+          ) : (
+            <div className="w-0 dark:secondary-dark-bg">Sidebar w-0</div>
+          )}
+          {/* here we are writing DRY code and first declaring what properties will be shared when activeMenu is true and false, then add the 1 diff property for each within the "${}" */}
+          <div
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            {/* here are all our nav links */}
+            <Routes></Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
