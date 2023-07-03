e4bc81f77871be708c7d9fc1ecce1ca6012dd5e0
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-07-03 00:42:49 -0700

    replaced ecommerce banner + building banner

--- react/react_admin_dashboard/src/App.js
+++ react/react_admin_dashboard/src/App.js
@@ -92,7 +92,7 @@ const App = () => {
                 <Route path="/stacked" element={<Stacked />} />
               </Routes>
             </div>
-            <Footer />
+            {/* <Footer /> */}
           </div>
         </div>
       </BrowserRouter>
