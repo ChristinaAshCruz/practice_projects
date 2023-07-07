56b4a888c7dc90fd6d1fb1f1f06708205e325d6e
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-07-07 12:51:55 -0700

    built cust bootstrap accordian + practice updates

--- react/react_admin_dashboard/src/pages/Ecommerce.jsx
+++ react/react_admin_dashboard/src/pages/Ecommerce.jsx
@@ -11,16 +11,16 @@ import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
   return (
-    <div className="mt-24">
+    <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center lg:bg-right">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center lg:bg-right pb-10">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
               <p className="text-2xl">$63,448.78</p>
             </div>
           </div>
-          <div className="mt-6">
+          <div className="mt-4">
             <Button
               color="white"
               bgColor="blue"
