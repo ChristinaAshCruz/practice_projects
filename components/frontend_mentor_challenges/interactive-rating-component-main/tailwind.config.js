34467a27ce73a99101d25d52ed75bf555886a987
Christina Ashley Cruz <christina.ash.cruz@gmail.com>
2023-10-31 13:41:06 -0700

    created thank you card element

--- components/frontend_mentor_challenges/interactive-rating-component-main/tailwind.config.js
+++ components/frontend_mentor_challenges/interactive-rating-component-main/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.{html,js}"],
   theme: {
     colors: {
       orange: "hsl(25, 97%, 53%)",
